09c1d74097e0b522e2a10aee8068fdbd6177b676
Jason Fricano <jason.fricano@gmail.com>
2020-09-22 16:50:11 +0800

    add draw(), range()

--- PointSET.java
+++ PointSET.java
@@ -3,6 +3,7 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.ResizingArrayBag;
 import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 // SubmitWrite a data type to represent a set of points in the unit square (all points have x- and y-coordinates between 0 and 1) using a 2d-tree to support efficient range search (find all of the points contained in a query rectangle) and nearest-neighbor search (find a closest point to a query point). 
@@ -36,12 +37,21 @@ public class PointSET {
 
   // draw all points to standard draw
   public void draw() {
-
+    for (Point2D point : set) {
+      double x = point.x();
+      double y = point.y();
+      StdDraw.point(x, y);
+      // StdDraw.text(x, y + .001, point.toString());
+    }
   }
 
   // all points that are inside the rectangle (or on the boundary)
   public Iterable<Point2D> range(RectHV rect) {
-    return null;
+    ResizingArrayBag<Point2D> pointsInRect = new ResizingArrayBag<>();
+    for (Point2D p : set)
+      if (rect.contains(p))
+        pointsInRect.add(p);
+    return pointsInRect;
   }
 
   // a nearest neighbor in the set to point p; null if the set is empty
@@ -55,18 +65,20 @@ public class PointSET {
     In in = new In(filename);
     PointSET brute = new PointSET();
 
+    // test empty set
     StdOut.println("size of set:\t" + brute.size());
     StdOut.println("empty set?:\t" + brute.isEmpty());
     StdOut.println();
 
+    // fill the set
     while (!in.isEmpty()) {
       double x = in.readDouble();
       double y = in.readDouble();
-      // if (x == String.eof() )
       Point2D p = new Point2D(x, y);
       brute.insert(p);
     }
 
+    // test filled set
     StdOut.println("Points added\n---------------------");
     int i = 0;
     for (Point2D point : brute.set)
@@ -78,5 +90,18 @@ public class PointSET {
     StdOut.println("contains (false):\t" + brute.contains(new Point2D(0, 3)));
     StdOut.println("contains (true):\t" + brute.contains(new Point2D(0.5, 1)));
     StdOut.println();
+
+    // test draw(), range()
+    RectHV rect = new RectHV(0.25, 0.25, 0.8, 1.0);
+
+    StdDraw.setCanvasSize(800, 800);
+    StdDraw.setPenRadius(0.005);
+    StdDraw.setScale(-.05, 1.05);
+    brute.draw();
+    rect.draw();
+
+    StdOut.println("Range in rect:");
+    for (Point2D point : brute.range(rect))
+      StdOut.println(point.toString());
   }
 }
