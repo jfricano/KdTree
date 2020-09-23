1ea95a6403b6b2fa687697a0cbea3b3b051f7897
Jason Fricano <jason.fricano@gmail.com>
2020-09-23 12:19:27 +0800

    add fields, Node class, isEmpty(), size()

--- KdTree.java
+++ KdTree.java
@@ -2,19 +2,42 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
+  private static final boolean VERTICAL   = true;
+  private static final boolean HORIZONTAL = false;
+  private Node root;
+
+  private class Node {
+    private Point2D point; // sorted by key
+    private boolean splitOrientation; // associated data
+    private Node lb, rt; // left and right subtrees
+    private int size; // number of nodes in subtree
+
+    public Node(Point2D p, boolean split, int sz) {
+      point = p;
+      splitOrientation = split;
+      lb = null;
+      rt = null;
+      size = sz;
+    }
+  }
+
   // construct an empty set of points
   public KdTree() {
-
+    
   }
 
   // is the set empty?
   public boolean isEmpty() {
-    return true;
+    return root == null;
   }
 
   // number of points in the set
   public int size() {
-    return 0;
+    return size(root);
+  }
+
+  private int size(Node x) {
+    return x == null ? 0 : x.size;
   }
 
   // add the point to the set (if it is not already in the set)
