8f2efc44e13104d4a96d80ce0fb6b02e476f3c32
Jason Fricano <jason.fricano@gmail.com>
2020-09-23 13:15:43 +0800

    add insert(), contains(), comparePoints()

--- KdTree.java
+++ KdTree.java
@@ -42,12 +42,37 @@ public class KdTree {
 
   // add the point to the set (if it is not already in the set)
   public void insert(Point2D p) {
+    if (p == null) throw new IllegalArgumentException("calls insert() with a null key");
+    root = insert(root, p, VERTICAL);
+  }
 
+  public Node insert(Node nd, Point2D p, boolean isVertical) {
+    if (nd == null) return new Node(p, isVertical, 1);
+    double cmp = comparePoints(p, nd.point, nd.splitOrientation);
+    // what about equals to zero??
+    if (cmp < 0) nd.lb = insert(nd.lb, p, !isVertical);
+    else         nd.rt = insert(nd.rt, p, !isVertical);
+    return nd;
+  }
+
+  private static double comparePoints(Point2D p, Point2D cmp, boolean isCmpVertical) {
+    if (isCmpVertical) return p.y() - cmp.y();
+    else               return p.x() - cmp.x();
   }
 
   // does the set contain point p?
   public boolean contains(Point2D p) {
-    return true;
+    return contains(root, p);
+  }
+  
+  private boolean contains(Node nd, Point2D p) {
+    if (p == null) throw new IllegalArgumentException("argument to contains() is null");
+    if (nd == null) return false;
+    if (p.equals(nd.point)) return true;
+    double cmp = comparePoints(p, nd.point, nd.splitOrientation);
+    // what about equals zero??
+    if (cmp < 0) return contains(nd.lb, p);
+    else         return contains(nd.rt, p);
   }
 
   // draw all points to standard draw
